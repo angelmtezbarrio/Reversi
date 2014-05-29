b30a8b4fb47ac55444cafed335653cf3a3acb855
angelillo <angelillo@192.168.11.50>
2014-05-29 21:10:58 +0200

    Añadido un solo método obtenerDesplazamientoFila()

--- reversiGit/src/juego/util/Direccion.java
+++ reversiGit/src/juego/util/Direccion.java
@@ -1,5 +1,12 @@
 package juego.util;
 
 public class Direccion {
-
+	/**
+	 * Método que obtiene el desplazamiento de la fila.
+	 * 
+	 * @return el desplazamiento de la fila.
+	 */
+	public int obtenerDesplazamientoFila() {
+		return desplazamientoFila;
+	}
 }
