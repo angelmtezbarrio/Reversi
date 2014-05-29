825955716f5b4aae359334599f56c79b0615e143
dam1t10 <dam1t10@IE14.jcolonia.local>
2014-05-29 19:24:24 +0200

    Paquete modelo. Versión 0.1 Clases: Celda, Color, Jugador, Pieza, Tablero

--- reversiGit/src/juego/modelo/Jugador.java
+++ reversiGit/src/juego/modelo/Jugador.java
@@ -1,5 +1,36 @@
+/**
+ * Paquete modelo.
+ */
 package juego.modelo;
 
+/**
+ * Nombre de la clase pública: Jugador.
+ * 
+ * @author Carolina Colina Zamorano.
+ * @since 1.0
+ * 
+ */
 public class Jugador {
 
+	/**
+	 * Atributo color de tipo Color.
+	 */
+	private Color color;
+	/**
+	 * Atributo nombre de tipo String.
+	 */
+	private String nombre;
+
+	/**
+	 * Constructor de la clase que crea los objetos nombre y jugador.
+	 * 
+	 * @param nombre
+	 *            nombre del jugador.
+	 * @param color
+	 *            color que corresponde a ese jugador.
+	 */
+	public Jugador(String nombre, Color color) {
+		this.nombre = nombre;
+		this.color = color;
+	}
 }
--- reversiGit/src/juego/modelo/Tablero.java
+++ reversiGit/src/juego/modelo/Tablero.java
@@ -1,5 +1,43 @@
+/**
+ * Paquete modelo.
+ */
 package juego.modelo;
 
+/**
+ * Listado de clases que hemos importado de otro paquete y que vamos a necesitar.
+ */
+import juego.modelo.Celda;
+import juego.modelo.Color;
+import juego.util.Dirección;
+
+/**
+ * Nombre de la clase pública: Tablero.
+ * 
+ * @author Carolina Colina Zamorano.
+ * @since 1.0
+ * 
+ */
 public class Tablero {
 
+	/**
+	 * Atributo celda de tipo Celda[][].
+	 */
+	private Celda[][] celdas;
+
+	/**
+	 * Constructor que crea el objeto celdas.
+	 * 
+	 * @param fila
+	 *            Se le pasa el número de filas.
+	 * @param columna
+	 *            Se le pasa el numero de columnas.
+	 */
+	public Tablero(int fila, int columna) {
+		celdas = new Celda[fila][columna];
+		for (int i = 0; i < fila; i++) {
+			for (int j = 0; j < columna; j++) {
+				celdas[i][j] = new Celda(i, j);
+			}
+		}
+	}
 }
